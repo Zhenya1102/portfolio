51654535d169a4298efcadb72b065d69204d53f4
Zhenya Luzhanski <luzhanskizhenya@gmail.com>
2023-07-31 19:26:50 +0300

    Commit changes to the block About and Home

--- src/main/componets/Text.tsx
+++ src/main/componets/Text.tsx
@@ -1,20 +1,29 @@
 import React from 'react';
 import style from './Text.module.css'
+
 type TextProps = {
     text: string
 }
 
-export const Text = (props:TextProps) => {
+export const Text = (props: TextProps) => {
     return (
         <div className={style.text}>
             <p>{props.text}</p>
-            <div>Education</div>
-            <div>Belarusian State University</div>
-            <div>Human Resources Management/Personnel Administration</div>
-            <div>Freelancer Life Style</div>
-            <div>Frontend Development</div>
-            <div>IT-Incubator</div>
-            <div>React Development</div>
+            <ul className={style.text__list}>
+                <li>Education:</li>
+                <li className={style.text__education}>
+                    <span>Belarusian State University:</span>
+                    <span>Human Resources Management/Personnel Administration</span>
+                </li>
+                <li className={style.text__education}>
+                    <span>Freelancer Life Style:</span>
+                    <span>Frontend Development</span>
+                </li>
+                <li className={style.text__education}>
+                    <span>IT-Incubator:</span>
+                    <span>React Development</span>
+                </li>
+            </ul>
         </div>
     )
 };
